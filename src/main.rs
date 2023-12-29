1adae11cb4228a7b9b8cd0211b092b511eb22d3c
crumblingstatue <radiantstatue@gmail.com>
2023-12-29 11:59:52 +0100

    Only parse "Desktop Entry" group of desktop files

--- src/main.rs
+++ src/main.rs
@@ -188,13 +188,35 @@ fn args_from_exec_string(exec: &str, arg: &OsStr) -> (String, Vec<OsString>) {
 
 type DesktopMap = HashMap<String, String>;
 
+enum ParseStatus {
+    // Initial status, trying to find desktop entry group
+    Init,
+    // Parsing desktop entry
+    DesktopEntry,
+}
+
 fn parse_desktop_file(path: impl AsRef<Path>) -> Result<DesktopMap, std::io::Error> {
     // .desktop files are UTF-8 according to spec
+    let mut status = ParseStatus::Init;
     let mut map = HashMap::new();
     let raw = std::fs::read_to_string(path)?;
     for line in raw.lines() {
-        if let Some((k, v)) = line.split_once('=') {
-            map.insert(k.trim().to_string(), v.trim().to_string());
+        match status {
+            ParseStatus::Init => {
+                if line.trim() == "[Desktop Entry]" {
+                    status = ParseStatus::DesktopEntry;
+                }
+            }
+            ParseStatus::DesktopEntry => {
+                // Another group is starting, we're not interested
+                if line.trim().starts_with('[') {
+                    return Ok(map);
+                }
+                // Collect key-value pairs
+                if let Some((k, v)) = line.split_once('=') {
+                    map.insert(k.trim().to_string(), v.trim().to_string());
+                }
+            }
         }
     }
     Ok(map)
